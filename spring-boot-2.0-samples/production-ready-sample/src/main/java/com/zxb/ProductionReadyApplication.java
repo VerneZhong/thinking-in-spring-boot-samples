73110877e3ef36a31ea654cc3464d589cdfca1f7
zhongxuebinwin8 <zhongxuebin171980062@gmail.com>
2019-12-16 15:35:40 +0800

    SpringBoot核心思想之理解Production-Ready特性

---
+++ spring-boot-2.0-samples/production-ready-sample/src/main/java/com/zxb/ProductionReadyApplication.java
@@ -0,0 +1,18 @@
+package com.zxb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * class
+ *
+ * @author Mr.zxb
+ * @date 2019-12-16 10:30
+ */
+@SpringBootApplication
+public class ProductionReadyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductionReadyApplication.class, args);
+    }
+}
