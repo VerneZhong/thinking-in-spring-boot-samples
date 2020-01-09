52621945c0158b77ab4d2b18af5d790924f16244
zhongxuebinwin8 <zhongxuebin171980062@gmail.com>
2020-01-09 14:48:48 +0800

    走向注解驱动编程之理解Spring注解属性抽象AnnotationAttributes（扩展与LinkedHashMap）

---
+++ spring-framework-samples/spring-framework-5.0.x-sample/src/main/java/com/zxb/thinking/in/spring/boot/samples/spring5/bootstrap/AnnotationAttributesBootstrap.java
@@ -0,0 +1,43 @@
+package com.zxb.thinking.in.spring.boot.samples.spring5.bootstrap;
+
+import com.zxb.thinking.in.spring.boot.samples.spring5.annotation.TransactionalService;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.core.annotation.AnnotationAttributes;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.reflect.AnnotatedElement;
+
+/**
+ * {@link AnnotationAttributes} 引导类 class
+ *
+ * @author Mr.zxb
+ * @date 2020-01-09 14:13
+ */
+public class AnnotationAttributesBootstrap {
+
+    public static void main(String[] args) {
+
+        AnnotatedElement annotatedElement = TransactionalService.class;
+
+        // 获取 @Service 注解属性独享
+        AnnotationAttributes serviceAttributes = AnnotatedElementUtils.getMergedAnnotationAttributes(annotatedElement
+                , Service.class);
+
+        // 获取 @TransactionalService 注解属性独享
+        AnnotationAttributes transactionalAttributes =
+                AnnotatedElementUtils.getMergedAnnotationAttributes(annotatedElement
+                , Transactional.class);
+
+        // 输出
+        print(serviceAttributes);
+
+        print(transactionalAttributes);
+    }
+
+    private static void print(AnnotationAttributes annotationAttributes) {
+        System.out.printf("注解 @%s 属性集合: \n", annotationAttributes.annotationType().getName());
+
+        annotationAttributes.forEach((name, value) -> System.out.printf("\t 属性 %s : %s \n", name, value));
+    }
+}
